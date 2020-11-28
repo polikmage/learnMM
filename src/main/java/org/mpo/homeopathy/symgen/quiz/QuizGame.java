e0072d5930a046b4bfecde6c17ad7e66d7be3651
Marek <polik.mage@seznam.cz>
2020-11-28 21:40:22 +0100

    quiz game v1

--- src/main/java/org/mpo/homeopathy/symgen/quiz/QuizGame.java
+++ src/main/java/org/mpo/homeopathy/symgen/quiz/QuizGame.java
@@ -9,15 +9,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class QuizGame {
-    //TODO v cyklu zobrazit 1 symptom a 4 leky jako moznosti, uzivatel bude psat pismena abcd.
-    // Spravna odpoved se zobrazi okamzite po napsani pismena a enter. datova struktura bude  pole Question objektu.
-    // Objekt Question ktery by obsahoval symptom, leky, oznaceni spravneho leku.
-    // Nutno mit metodu pro generovani 3 nespravnych odpovedi
+    //TODO zkontrolovat generovani doplnkovych odpovedi, zprehlednit quiz, zobrazit spravny lek a ne pismeno, zkontrolovat generator symptomu, je tam prilis casto nat mur
 
     QuizGenerator quizGenerator;
     List<Remedy> remedies;
     List<String> symptoms;
     private List<Question> questions;
+    private int correctAnswersNumber = 0;
 
 
     public QuizGame(List<Remedy> remedies, List<String> symptoms) {
@@ -31,19 +29,39 @@ public class QuizGame {
         ConsoleView.showPreText();
 
         for (int i = 0; i < questions.size(); i++) {
-            ConsoleView.showQuestion(questions.get(i),i);
+            ConsoleView.Answer correctAnswer= ConsoleView.showQuestion(questions.get(i),i);
+            ConsoleView.showDescription();
+            String key = readKey();
+            if(getResult(key,correctAnswer)){
+                ConsoleView.showResultAnswer(correctAnswer);
+            }else {
+                ConsoleView.showWrongAnswer(correctAnswer);
+            }
+
+
+
+
         }
 
-        ConsoleView.showPostText();
+        ConsoleView.showPostText(correctAnswersNumber,numOfQuestions);
+    }
+
+    private Boolean getResult(String key, ConsoleView.Answer answer) {
+        if (key.equalsIgnoreCase(answer.toString())){
+            correctAnswersNumber++;
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
-    public static String readKey(){
+    public String readKey(){
         Scanner scan= new Scanner(System.in);
 
         //For string
         String text= scan.nextLine();
-        System.out.println(text);
+        //System.out.println(text);
 
         //for int
         /*
--- src/main/java/org/mpo/homeopathy/symgen/quiz/view/ConsoleView.java
+++ src/main/java/org/mpo/homeopathy/symgen/quiz/view/ConsoleView.java
@@ -6,38 +6,71 @@ import java.util.List;
 
 public class ConsoleView {
 
+    public static void showResultAnswer(Answer correctAnswer) {
+        printLnText("TREFA! Spravna odpoved je: "+correctAnswer);
+    }
+
+    public static void showWrongAnswer(Answer correctAnswer) {
+        printLnText("Chyba spravna odpoved byla: "+correctAnswer);
+    }
+
+    public enum Answer {
+        a(0), b(1), c(2), d(3);
+
+        private int value;
+
+        Answer(int value) {
+            this.value = value;
+        }
+
+        public static Answer enumFromInt(int value) {
+            for (Answer a : Answer.values()) {
+                if (a.value == value) {
+                    return a;
+                }
+            }
+            return null;
+        }
+    }
+
     static final String liner = "*****************************************************";
 
-    public static void showPreText(){
+    public static void showPreText() {
         printLnText("Vitejte v Materia Medica quizu, ceka vas prirazovani 20 symptomu k 4 lekum kdz jeden a vice je spravnych");
     }
-    public static void showPostText(){
-        printLnText("Vyborne vase skore je:");
+
+    public static void showPostText(int correctAnswersNumber,int num) {
+        printLnText("Vyborne vase skore je: "+ correctAnswersNumber +" z "+num);
     }
 
 
-
-    public static void showQuestion(Question question, int i){
+    public static Answer showQuestion(Question question, int i) {
 
         printLnText(liner);
-        printLnText("Otazka cislo: "+i);
+        printLnText("Otazka cislo: " + i);
         printLnText(question.getSymptom());
         printLnText("Mozne odpovedi");
-        printAnswers(question.getAllRemedies());
 
+        return printAnswers(question);
 
     }
 
-    private static void printLnText(String text){
+    private static void printLnText(String text) {
         System.out.println(text);
     }
 
-    private static void printAnswers(List<String> remedies){
-        System.out.print("A: "+ remedies.get(0)+", ");
-        System.out.print("B: "+ remedies.get(1)+", ");
-        System.out.print("C: "+ remedies.get(2)+", ");
-        System.out.print("D: "+ remedies.get(3)+", \n");
+    private static Answer printAnswers(Question question) {
+        List<String> remedies = question.getAllRemedies();
+        System.out.print(Answer.a + ": " + remedies.get(0) + ", ");
+        System.out.print(Answer.b + ": " + remedies.get(1) + ", ");
+        System.out.print(Answer.c + ": " + remedies.get(2) + ", ");
+        System.out.print(Answer.d + ": " + remedies.get(3) + ", \n");
+        int indexOfCorrectRemedy = remedies.indexOf(question.getCorrectRemedy());
+        return Answer.enumFromInt(indexOfCorrectRemedy);
     }
 
 
+    public static void showDescription() {
+        System.out.print("Zvol [a,b,c,d] a enter : ");
+    }
 }
